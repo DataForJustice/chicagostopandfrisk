8b52b9f5f795bfa643af1335dcd645b022eafb13
pa-w <paw@paw.mx>
2017-04-09 01:12:32 -0400

    Added legends and labels, hover events, clicks, etc

--- js/policing.js
+++ js/policing.js
@@ -5,22 +5,21 @@ $(document).ready (function () {
 			stops: function (args) { 
 				if (!args) args = {};
 				if (!args.nestvalue) args.nestvalue = {};
-				var nests = args.nests ? args.nests : ["beat"], columns = args.columns ? args.columns : ["total"] ;
+				var nests = args.nests ? args.nests : ["beat"], column = args.column ? args.column : "total";
 				var vals = [];
 				var cb = function (v, col) { 
-					if (columns.indexOf (col) !== -1) {
-						return d3.sum (v);
-					}
+					return d3.sum (v);
 				}
-				var nest = new Nestify (this.data.stops, nests, columns, cb).data;
+
+				var nest = new Nestify (this.data.stops, nests, this.data.stops.columns, cb).data;
 				var extent = nest.extent (function (a) { 
 					for (var i in nests) { 
 						if (args.nestvalue [nests [i]]) {	
 							a = a.values [args.nestvalue [nests [i]]];
 						}
 					}
-					if (!a [columns [0]]) a = a.values;
-					return parseInt (a [columns [0]].value); 
+					if (!a [column]) a = a.values;
+					return parseInt (a [column].value); 
 				});
 				return {
 					nest: nest,
@@ -35,8 +34,10 @@ $(document).ready (function () {
 				},
 				stops: function (beat, args, preq) { 
 					if (!args) args = {};
-					var nests = args.nests ? args.nests : ["beat"], columns = args.columns ? args.columns : ["total"] ;
+					var nests = args.nests ? args.nests : ["beat"], column = args.column ? args.column : ["total"] ;
 					var obj = preq.nest;
+					var beatId = beat.properties [nests [0]];
+					var beatObj = preq.nest [beatId]; // "beat" should always be the first pivot
 					for (var x in nests) {
 						var nestval = null;
 						if (args.nestvalue) {
@@ -44,8 +45,36 @@ $(document).ready (function () {
 						}
 						obj = obj [nestval ? nestval : beat.properties [nests [x]]]; 
 					}
+					var ks = ["BLACK", "HISPANIC", "WHITE"], cs = ["white", "black", "hispanic", "other"];
 					if (obj) {
-						return { "class": "m_" + preq.scale (obj [columns [0]].value) };
+						var parse = [
+							{"parse": "#show_legend,#clear_beats"},
+							{"control_element": ".beat.beat_" + beatId, "element_add_class": "selected"}
+						]
+
+						for (var xk in ks) {
+							for (var ck in cs) {
+								if (beatObj [ks [xk]] && beatObj [ks [xk]] [cs [ck]]) {
+									var className = ".legend." + ks [xk].toLowerCase () + "_on_" + cs [ck];
+									var bOj = beatObj [ks [xk]] [cs [ck]];
+									if (bOj.value) {
+										parse.push ({"control_element": className, "element_show": true });
+										parse.push ({"control_element": className + " .label", "element_text": bOj.value});
+									}
+								}
+							}
+						}
+						parse.push ({control_element: ".beat_id", element_text: beatId});
+						if (preq.nest [beatId].total) { 
+							parse.push ({control_element: ".total_stops", element_text: preq.nest [beatId].total.value});
+						} else {
+							var total = preq.nest [beatId].sum (function (a) { return a.values.total.value; });
+							parse.push ({"control_element": ".total_stops", "element_text": total});
+						}
+						var data = {
+							"parse": parse
+						};
+						return { "class": "beat beat_" + beatId + " m_" + preq.scale (obj [column].value), "data": data};
 					}
 				}
 			}
